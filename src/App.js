71f1857282bfa8436a0214cea3c80b9f4c64e9a2
Nandeep Nagarkar <nandeep@gmail.com>
2020-05-11 16:38:50 -0700

    Checkpoint 1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
+import { onError } from "./libs/errorLib";
 
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
@@ -22,7 +23,7 @@ function App() {
     }
     catch (e) {
       if (e !== 'No current user') {
-        alert(e);
+        onError(e);
       }
     }
 
--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
             </Route>
             <Route exact path="/login">
                 <Login />
