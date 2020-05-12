fbdd2915c688c4ec30782432e0042f8c4a09e036
Nandeep Nagarkar <nandeep@gmail.com>
2020-05-11 18:51:51 -0700

    Note upload to S3 done

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from "react";
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
-// import { onError } from "../libs/errorLib";
+import { onError } from "../libs/errorLib";
 import config from "../config";
 import "./NewNote.css";
+import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 
 export default function NewNote() {
     const file = useRef(null);
-    // const history = useHistory();
+    const history = useHistory();
     const [content, setContent] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
@@ -25,13 +27,30 @@ export default function NewNote() {
 
         if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
             alert(
-                `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-                1000000} MB.`
+                `Please pick a file smaller than ${
+                config.MAX_ATTACHMENT_SIZE / 1000000
+                } MB.`
             );
             return;
         }
 
         setIsLoading(true);
+
+        try {
+            const attachment = file.current ? await s3Upload(file.current) : null;
+
+            await createNote({ content, attachment });
+            history.push("/");
+        } catch (e) {
+            onError(e);
+            setIsLoading(false);
+        }
+    }
+
+    function createNote(note) {
+        return API.post("notes", "/notes", {
+            body: note
+        });
     }
 
     return (
