0302eb0eba163ed2f3689f3493cc2a50aa375e69
Nandeep Nagarkar <nandeep@gmail.com>
2020-05-11 22:45:15 -0700

    Notes CRUD Completed

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">Scratch</Link>
+            <Link to="/">NavBar</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default function Routes() {
     return (
@@ -17,6 +18,9 @@ export default function Routes() {
             </Route>
             <Route exact path="/notes/new">
                 <NewNote />
+            </Route>
+            <Route exact path="/notes/:id">
+                <Notes />
             </Route>
             <Route exact path="/login">
                 <Login />
