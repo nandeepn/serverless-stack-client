453d17101c79c49b6dd9a8c83aa7c7d4df0ba58d
Nandeep Nagarkar <nandeep@gmail.com>
2020-05-15 22:00:42 -0700

    Adding error reporting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
 import { onError } from "./libs/errorLib";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
@@ -66,11 +67,11 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <AppContext.Provider
-          value={{ isAuthenticated, userHasAuthenticated }}
-        >
-          <Routes />
-        </AppContext.Provider>
+        <ErrorBoundary>
+          <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+            <Routes />
+          </AppContext.Provider>
+        </ErrorBoundary>
       </div>
     )
   );
--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,41 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.init({ dsn: "https://f85c29b434e3457fb4338444823237ae@o392436.ingest.sentry.io/5239915" });
+}
+
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.withScope((scope) => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
+}
+
 export function onError(error) {
+    let errorInfo = {};
     let message = error.toString();
 
     // Auth errors
     if (!(error instanceof Error) && error.message) {
+        errorInfo = error;
         message = error.message;
+        error = new Error(message);
+        // API errors
+    } else if (error.config && error.config.url) {
+        errorInfo.url = error.config.url;
     }
+
+    logError(error, errorInfo);
 
     alert(message);
 }
